53bdd360072ade64cddc2fb8389b9c03a45921bc
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2025-01-06 22:03:47 -0300

    inicializando o projeto com a funcionalidade filtro funcionando

---
+++ 20-filters/app.js
@@ -0,0 +1,36 @@
+let filteredProducts = [...products];
+
+const productsContainer = document.querySelector('.products-container');
+
+const displayProducts = () =>{
+    // if statement
+
+    productsContainer.innerHTML = filteredProducts.map(({id, title, image, price}) =>{
+        return `<article class="product" data-id="${id}">
+          <img
+            src="${image}"
+            class="product-img img"
+            alt=""
+          />
+          <footer>
+            <h5 class="product-name">${title}</h5>
+            <span class="product-price">${price}</span>
+          </footer>
+        </article>`;
+    })
+    .join('')
+};
+
+displayProducts();
+// tet filter
+
+const form = document.querySelector('.input-form');
+const searchInput = document.querySelector('.search-input');
+
+form.addEventListener('keyup', () =>{
+    const inputValue = searchInput.value;
+    filteredProducts = products.filter((product) => {
+        return product.title.toLowerCase().includes(inputValue);
+    });
+    displayProducts();
+})
