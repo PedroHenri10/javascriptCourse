5d659abea9748f72102dde60fd63b29348b96470
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2025-01-07 22:13:49 -0300

    projeto finalizado

--- 20-filters/app.js
+++ 20-filters/app.js
@@ -4,6 +4,10 @@ const productsContainer = document.querySelector('.products-container');
 
 const displayProducts = () =>{
     // if statement
+    if(filteredProducts.length < 1) {
+      productsContainer.innerHTML = `<h6>Sorry, no products  matched your search</h6`;
+      return;
+    }
 
     productsContainer.innerHTML = filteredProducts.map(({id, title, image, price}) =>{
         return `<article class="product" data-id="${id}">
@@ -33,4 +37,36 @@ form.addEventListener('keyup', () =>{
         return product.title.toLowerCase().includes(inputValue);
     });
     displayProducts();
+})
+
+// filter buttons
+
+const companiesDOM = document.querySelector('.companies');
+
+const displayButtons = () => {
+  const buttons = [
+    'all', ...new Set(products.map((product) => product.company)),
+  ];
+  companiesDOM.innerHTML = buttons.map((company) => {
+    return `<button class="company-btn" data-id="${company}">
+    ${company}</button>`;
+  })
+  .join('');
+};
+
+displayButtons();
+
+companiesDOM.addEventListener('click', (e) => {
+  const el = e.target;
+  if(el.classList.contains('company-btn')){
+    if(el.dataset.id === 'all'){
+      filteredProducts = [...products];
+    }else{
+      filteredProducts = products.filter((product) => {
+        return product.company === el.dataset.id;
+      });
+    }
+    searchInput.value = '';
+    displayProducts();
+  }
 })
