2942b0ff63bbafcd96b054edc699ac0513e63bbc
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2025-01-23 22:55:41 -0300

    funcionalidade filtro

--- 29-comfy-store/src/filters/search.js
+++ 29-comfy-store/src/filters/search.js
@@ -1,5 +1,27 @@
 import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
-const setupSearch = () => {};
+const setupSearch = (store) => {
+    const form = getElement('.input-form');
+    const nameInput = getElement('.search-form');
+    form.addEventListener('keyup', function () {
+        const value = nameInput.value;
+        if(value){
+            const newStore = store.filter((product) => {
+                let {name} = product;
+                name = name.toLowerCase();
+                if(name.startsWith(value)){
+                    return product;
+                }
+            });
+            display(newStore, getElement('.products-container'));
+            if(newStore.length < 1){
+                const products = getElement('.products-container');
+                products.innerHTML = `<h3 class="filter-error">sorry, no products matched your search</h3>`
+            }
+        }else{
+            display(Store, getElement('.products-container'));
+        }
+    })
+};
 
 export default setupSearch;
