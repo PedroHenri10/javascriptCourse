d61c5be0e88a0d6af4e40477ddfabcbd49e2813a
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2025-01-25 23:21:49 -0300

    single product e configuraçao carrinho

---
+++ 29-comfy-store/29-comfy-store/src/cart/setupCart.js
@@ -0,0 +1,26 @@
+// import
+import {
+  getStorageItem,
+  setStorageItem,
+  formatPrice,
+  getElement,
+} from '../utils.js';
+import { openCart } from './toggleCart.js';
+import { findProduct } from '../store.js';
+import addToCartDOM from './addToCartDOM.js';
+// set items
+
+const cartItemCountDOM = getElement('.cart-item-count');
+const  cartItemsDOM = getElement('.cart-items');
+const cartTotalDOM = getElement('.cart-total');
+
+let cart = getStorageItem('cart')
+
+export const addToCart = () => {
+  openCart();
+};
+
+const init = () => {
+  
+}
+init();
